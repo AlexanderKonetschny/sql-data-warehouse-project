10764deb9f1f92e353ce8ba301984d348fdd4b83
Alexander Konetschny <Alexander.Konetschny@gmx.de>
2025-08-11 11:12:03 +0200

    Create init_dwh.sql

---
+++ scripts/init_dwh.sql
@@ -0,0 +1,32 @@
+/*
+======================================================================================
+Create Database
+======================================================================================
+Script Purpose:
+    This script creates a new database named 'dwh_project' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. At the end, a table ist created to log
+    the loading process in the bronze layer.
+WARNING:
+    Running this script will drop the entire 'dwh_project' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+======================================================================================
+*/
+
+-- delete database if exists
+DROP DATABASE IF EXISTS dwh_project;
+
+-- create database
+CREATE DATABASE dwh_project; 
+
+-- select database
+USE dwh_project; 
+
+-- create table for logging loading process
+CREATE TABLE IF NOT EXISTS bronze_log_load (
+    log_id 					        INT AUTO_INCREMENT PRIMARY KEY,
+    table_name 			      	VARCHAR(255),
+    load_start			      	DATETIME,
+    load_end				        DATETIME,
+    load_duration_seconds 	INT
+);
