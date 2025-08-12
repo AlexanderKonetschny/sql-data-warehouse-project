aea3684f29b6eed1b689f3bb1f090c53f361f0bd
Alexander Konetschny <Alexander.Konetschny@gmx.de>
2025-08-12 11:11:46 +0200

    Update load_bronze.sql
    When there is an empty string in an integer value, there should be now loaded as NULL not 0.

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -24,7 +24,10 @@ SET @load_start = NOW();
 LOAD DATA LOCAL INFILE 'C:/Users/AKony/OneDrive/Desktop/mySQL-data-warehouse-project/datasets/source_crm/cust_info.csv' # path to files
 INTO TABLE bronze_crm_cust_info
 FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+    IGNORE 1 LINES
+(@cst_id, cst_key, cst_firstname, cst_lastname, cst_marital_status, cst_gender, cst_create_date)	-- INT set NULL if empty
+	SET cst_id = NULLIF(@cst_id, '');
+;
 
 SET @load_end = NOW();
 SET @load_duration_seconds = TIMESTAMPDIFF(SECOND, @load_start, @load_end);
@@ -44,7 +47,12 @@ SET @load_start = NOW();
 LOAD DATA LOCAL INFILE 'C:/Users/AKony/OneDrive/Desktop/mySQL-data-warehouse-project/datasets/source_crm/prd_info.csv' # path to files
 INTO TABLE bronze_crm_prd_info
 FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+    IGNORE 1 LINES
+(@prd_id, prd_key, prd_nm, @prd_cost, prd_line, prd_start_dt, prd_end_dt) -- INT set NULL if empty
+SET 
+	prd_id =	 NULLIF(@prd_id, ''), 
+	prd_cost =	 NULLIF(@prd_cost, '')
+;
 
 SET @load_end = NOW();
 SET @load_duration_seconds = TIMESTAMPDIFF(SECOND, @load_start, @load_end);
@@ -63,7 +71,17 @@ SET @load_start = NOW();
 LOAD DATA LOCAL INFILE 'C:/Users/AKony/OneDrive/Desktop/mySQL-data-warehouse-project/datasets/source_crm/sales_details.csv' # path to files
 INTO TABLE bronze_crm_sales_details
 FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+    IGNORE 1 LINES
+(sls_ord_num, sls_prd_key, @sls_cust_id, @sls_order_dt, @sls_ship_dt, @sls_due_dt, @sls_sales, @sls_quantity, @sls_price) -- INT set NULL if empty
+SET 
+	sls_cust_id =	 NULLIF(@sls_cust_id, ''), 
+	sls_order_dt =	 NULLIF(@sls_order_dt, ''),
+    sls_ship_dt =	 NULLIF(@sls_ship_dt, ''),
+    sls_due_dt =	 NULLIF(@sls_due_dt, ''),
+    sls_sales =	 	 NULLIF(@sls_sales, ''),
+    sls_quantity =	 NULLIF(@sls_quantity, ''),
+    sls_price =		 NULLIF(@sls_price, '')
+;
 
 SET @load_end = NOW();
 SET @load_duration_seconds = TIMESTAMPDIFF(SECOND, @load_start, @load_end);
